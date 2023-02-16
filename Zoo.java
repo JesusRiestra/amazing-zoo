9a3f68d32bb50520946ea76e4d151cbbc3df460d
Silvia <silviadelucasalv@gmail.com>
2023-02-16 11:08:22 +0100

    Incluye los métodos getter y setter para cada uno de los dos atributos de la segunda clase

--- Zoo.java
+++ Zoo.java
@@ -6,4 +6,20 @@ public class Zoo {
         ciudad = nuevaCiudad;
         this.tieneAcuario = tieneAcuario;
     }
+    
+    public void setCiudad (String nuevaCiudad) {
+        ciudad = nuevaCiudad;
+    }
+
+    public String getCiudad (){
+        return ciudad;
+    }
+
+    public void setacuario(boolean acuarioNuevo) {
+        tieneAcuario = acuarioNuevo;
+    }
+
+    public boolean getTieneAcuario (){
+        return tieneAcuario;
+    }
 }
