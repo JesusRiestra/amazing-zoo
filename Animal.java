c6ef7ab9e3224187e9d9450a8166d923a734f548
JesusRiestra <jriestraricote@gmail.com>
2023-02-16 11:17:42 +0100

    Incluye el constructor de la primera clase

--- Animal.java
+++ Animal.java
@@ -2,4 +2,10 @@ public class Animal {
     private String nombre;
     private int edad;
     private Zoo zoo;
+
+    public Animal(String nuevoNombre, int nuevaEdad, Zoo zoo ) {
+        nombre = nuevoNombre;
+        edad = nuevaEdad;
+        zoo = zoo;
+    }
 }
