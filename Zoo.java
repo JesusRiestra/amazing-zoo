5cff69b034a02349574f9fd0bfd349089eb59c83
JesusRiestra <jriestraricote@gmail.com>
2023-02-16 11:05:03 +0100

    Incluye la estructura, los dos atributos y el constructor de la segunda clase

---
+++ Zoo.java
@@ -0,0 +1,9 @@
+public class Zoo {
+    private String ciudad;
+    private boolean tieneAcuario;
+
+    public Zoo(String nuevaCiudad, boolean tieneAcuario) {
+        ciudad = nuevaCiudad;
+        this.tieneAcuario = tieneAcuario;
+    }
+}
